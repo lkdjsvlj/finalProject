569cbf71e03523b9df7eeafda173b49bdfdeeb70
laurie <laurie.moss@icloud.com>
2024-07-26 13:52:35 +0100

    Initial Commit

---
+++ finalProject/finalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectApp.swift
+//  finalProject
+//
+//  Created by Scholar on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
